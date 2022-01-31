2a510548a4768833511d5d8240422176be67df89
dariomontani <dariomontani@gmail.com>
2022-01-31 17:56:59 +0100

    Snack 5

--- index5.php
+++ index5.php
@@ -2,6 +2,10 @@
 // ## Snack 5
 // Prendere un paragrafo abbastanza lungo, contenente diverse frasi. Prendere il paragrafo e suddividerlo in tanti paragrafi. Ogni punto un nuovo paragrafo.
 
+$paragrafo = 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime numquam, enim facilis fugiat nam aut veritatis ratione ut voluptas? Assumenda doloribus culpa ad voluptatibus ullam dolore consequuntur accusamus fugiat impedit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa officiis alias non asperiores perspiciatis modi at vero distinctio fuga iste nobis harum, necessitatibus deleniti ipsam commodi dicta rerum earum laboriosam. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum itaque corrupti deserunt voluptatem architecto sequi ratione iure provident nostrum sit, odit, incidunt eos! Expedita odit impedit recusandae adipisci ex ea? Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni provident laboriosam quos eum! Neque commodi atque, error ea cumque dolore quae, unde nulla nisi est iure illum delectus iste sapiente! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae earum aperiam iusto, minus commodi doloremque illo, aliquid necessitatibus vero enim saepe repellat cum optio amet ut aspernatur, accusantium iste veritatis!';
+
+$paragrafoDiviso = str_replace(".", ".<br>", $paragrafo);
+
 ?>
 
 <!DOCTYPE html>
@@ -13,6 +17,6 @@
     <title>Snack 5</title>
 </head>
 <body>
-    
+    <p><?php echo $paragrafoDiviso; ?></p>
 </body>
 </html>
